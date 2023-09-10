d6dd2289465912b5fb21f34d6d8fdb70e38b806d
Colton Randall <coltonrandall.nz@gmail.com>
2023-09-11 10:16:14 +1200

    Add play round logic

--- logic.js
+++ logic.js
@@ -9,4 +9,33 @@ function getComputerChoice() {
 	}
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+	while (playerSelection != computerSelection) {
+		playerSelection = playerSelection.toLowerCase();
+		console.log("player selection is " + playerSelection);
+
+		if (playerSelection == "rock" && computerSelection == "scissors") {
+			return "You Win! Rock beats Scissors";
+		} else if (playerSelection == "rock" && computerSelection == "paper") {
+			return "You lose! Paper beats rock";
+		}
+
+		if (playerSelection == "scissors" && computerSelection == "paper") {
+			return "You Win! Scissors beats paper";
+		} else if (playerSelection == "scissors" && computerSelection == "rock") {
+			return "You Lose! Rock beats paper";
+		}
+
+		if (playerSelection == "paper" && computerSelection == "rock") {
+			return "You Win! Paper beats rock";
+		} else if (playerSelection == "paper" && computerSelection == "scissors") {
+			return "You Lose! Scissors beats paper";
+		}
+	}
+	return "It's a tie!";
+}
+
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+console.log("computer selection is " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
