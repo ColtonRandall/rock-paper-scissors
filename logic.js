17a35ea428bbbd649194e4db1efcb825aad6b6e6
Colton Randall <coltonrandall.nz@gmail.com>
2023-10-03 22:19:58 +1300

    Begin refactor to convert this to having a UI

--- logic.js
+++ logic.js
@@ -1,64 +1,83 @@
-function getComputerChoice() {
-	let choice = Math.round(Math.random() * 2);
-	if (choice === 0) {
-		return "rock";
-	} else if (choice === 1) {
-		return "paper";
-	} else if (choice === 2) {
-		return "scissors";
-	}
-}
 
-function playRound(playerSelection, computerSelection) {
-	while (playerSelection != computerSelection) {
-		playerSelection = playerSelection.toLowerCase();
-		console.log("player selection is " + playerSelection);
 
+/* TODO:
+	- Add logic to each button (as done for Rock below)
+	- Fix playRound() logic to not hard code playerSelection or computerSelection
+		within the method lines 35/36)
+	- Add div to show current score for each round.
+*/
+
+const displayResults = document.querySelector('#results');
+
+//Add event listeners to each button that call playRound()
+const chooseRock = document.querySelector('#rock');
+	chooseRock.addEventListener('click', () => {
+		playRound();
+		console.log('success');
+	});
+
+
+
+// function getComputerChoice() {
+// 	let choice = Math.round(Math.random() * 2);
+// 	if (choice === 0) {
+// 		return "rock";
+// 	} else if (choice === 1) {
+// 		return "paper";
+// 	} else if (choice === 2) {
+// 		return "scissors";
+// 	}
+// }
+
+
+
+function playRound() {
+	let playerSelection = 'rock';
+	let computerSelection = 'paper';
 		if (playerSelection == "rock" && computerSelection == "scissors") {
-			return "You Win! Rock beats Scissors";
+			displayResults.textContent = "You Win! Rock beats Scissors";
 		} else if (playerSelection == "rock" && computerSelection == "paper") {
-			return "You Lose! Paper beats rock";
+			displayResults.textContent =  "You Lose! Paper beats rock";
 		}
 
 		if (playerSelection == "scissors" && computerSelection == "paper") {
-			return "You Win! Scissors beats paper";
+			displayResults.textContent =  "You Win! Scissors beats paper";
 		} else if (playerSelection == "scissors" && computerSelection == "rock") {
-			return "You Lose! Rock beats paper";
+			displayResults.textContent =  "You Lose! Rock beats paper";
 		}
 
 		if (playerSelection == "paper" && computerSelection == "rock") {
-			return "You Win! Paper beats rock";
+			displayResults.textContent =  "You Win! Paper beats rock";
 		} else if (playerSelection == "paper" && computerSelection == "scissors") {
-			return "You Lose! Scissors beats paper";
-		}
+			displayResults.textContent =  "You Lose! Scissors beats paper";
+		
 	}
 	return "It's a tie!";
 }
 
-function game() {
-	let playerPoints = 0;
-	let computerPoints = 0;
+// function game() {
+// 	let playerPoints = 0;
+// 	let computerPoints = 0;
 
-	while (playerPoints < 5 && computerPoints < 5) {
-		let playerSelection = prompt("📃 or ✂️ or 🪨?");
-		let computerSelection = getComputerChoice();
-		let roundWinner = playRound(playerSelection, computerSelection);
+// 	while (playerPoints < 5 && computerPoints < 5) {
+// 		let playerSelection = prompt("📃 or ✂️ or 🪨?");
+// 		let computerSelection = getComputerChoice();
+// 		let roundWinner = playRound(playerSelection, computerSelection);
 
-		if (roundWinner.includes("You Win!")) {
-			playerPoints++;
-		} else if (roundWinner.includes("You Lose!")) {
-			computerPoints++;
-		}
-		console.log(roundWinner);
-		console.log("player points " + playerPoints);
-		console.log("computer points " + computerPoints);
-	}
+// 		if (roundWinner.includes("You Win!")) {
+// 			playerPoints++;
+// 		} else if (roundWinner.includes("You Lose!")) {
+// 			computerPoints++;
+// 		}
+// 		console.log(roundWinner);
+// 		console.log("player points " + playerPoints);
+// 		console.log("computer points " + computerPoints);
+// 	}
 
-	if (computerPoints == 5) {
-		console.log("Game over! Computer Wins! 🤖");
-	} else if (playerPoints == 5) {
-		console.log("Game over! You Win! 🎉🎉🎉");
-	}
-}
+// 	if (computerPoints == 5) {
+// 		console.log("Game over! Computer Wins! 🤖");
+// 	} else if (playerPoints == 5) {
+// 		console.log("Game over! You Win! 🎉🎉🎉");
+// 	}
+// }
 
-game();
